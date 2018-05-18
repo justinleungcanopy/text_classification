2be09343cae4f74d305cbdb010d7ad3036e35844
Justin_Leung <justin.leung@canopy.cloud>
2018-05-18 18:35:00 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,9 @@
+import sklearn
+import nltk
+import random
+from nltk.corpus import movie_reviews
+
+
+for category in categories:
+	for fileid in data.category:
+		somelist.append(list(word,category))
